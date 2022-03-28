1e4bbb1ff3f2588d7adc793874931a468c411ee7
dipika <63471537+dipika101@users.noreply.github.com>
2022-03-28 22:07:19 +0530

    initial folders

---
+++ job portal/server/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
+import applicantsRoutes from './routes/applicants.js';
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+app.use('/users',userRoutes);
+app.use('/applicants',applicantsRoutes);
+
+app.use('/',(req,res)=>{
+  res.send("APP IS RUNNING");
+});
+
+
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+// mongoose.set('useFindAndModify', false);
